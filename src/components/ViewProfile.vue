fc5dabbc86d764135d07d5e35c2ac709aee7e294
Charles McQuain <charlesmcquain@gmail.com>
2018-07-24 21:43:18 -0700

    Created ViewProfile component

---
+++ src/components/ViewProfile.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="view-profile">
+    <h2>Profile</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ViewProfile",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+</style>
